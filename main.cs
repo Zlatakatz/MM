9457fe4ccab07148d5f6ca8162bc9487715bed22
Катя <seijuro_1703@mail.ru>
2023-02-15 02:38:55 +0300

    Now Hello a personalized.

--- main.cs
+++ main.cs
@@ -5,7 +5,7 @@ class Git
 {
 static void Main(string[] args)
 {
-System.Console.WriteLine("Hello Git!");
+System.Console.WriteLine("Enter your name: ");
 string name = Console.ReadLine();
 System.Console.WriteLine(name.Length); //debug line
 System.Console.WriteLine($"Hello {name}!");
